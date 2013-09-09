0b970419a695f599d9347c2894859f783dee22cc
Michael Bruce <bruce@jhereg.net>
2013-09-09 02:20:44 -0400

    Adding homeshick linking to bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,4 @@ mkdir -p ~/tmp
 
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 $HOME/.homesick/repos/homeshick/home/.homeshick clone -b reprehensible/dotfiles
+$HOME/.homesick/repos/homeshick/home/.homeshick link
