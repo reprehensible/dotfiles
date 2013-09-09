bc30f398252eecc04e6904496b160d8bf4362097
Michael Bruce <bruce@jhereg.net>
2013-09-09 02:18:41 -0400

    Adding helpful things to boostrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p ~/tmp
+
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-$HOME/.homesick/repos/homeshick/home/.homeshick clone reprehensible/dotfiles
+$HOME/.homesick/repos/homeshick/home/.homeshick clone -b reprehensible/dotfiles
